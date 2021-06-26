a8807f93e6256b4eb0ed9b346935fd0bcd4d668b
Jyotirmaya Das <jyotirmaya12@gmail.com>
2021-06-26 22:10:28 +0700

    Formatted

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,25 +21,25 @@ public class DemoApplication {
 	public void method3() {
 
 	}
-	
+
 	public void method4() {
 
 	}
-	
+
 	public void method5() {
 
 	}
-	
+
 	public void method6() {
 
 	}
-	
+
 	public void method7() {
 
 	}
-	
-  public void method8() {
+
+	public void method8() {
 
 	}
-  
+
 }
